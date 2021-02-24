c60e2931fee08976202ffdf7d1759e15ec392625
Joakim Flink <39080802+abbjoafli@users.noreply.github.com>
2021-02-24 15:36:47 +0100

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,6 @@ import Vuex from 'vuex'
 import { vuexfireMutations } from 'vuexfire'
 import pokemons from './pokemons'
 import user from './user'
-import createPersistedState from "vuex-persistedstate"; //Denna gör så att vue inte tappar värden när du uppdaterar.
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
